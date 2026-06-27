d31ed7758f1c48b6b5057cf4e662c09c7bdf6a47
RudeVal <vrudellifacu@gmail.com>
2026-06-27 17:05:54 -0300

    Calculadora.cs terminado

--- CalculadoraHistorial/Calculadora.cs
+++ CalculadoraHistorial/Calculadora.cs
@@ -6,59 +6,81 @@ namespace EspacioCalculadora
         private double dato; //campo dato
 
         //agregamos lista de objetos de tipo Operacion
-        //private List<Operacion> historial; 
+        private List<Operacion> historial; 
 
-        //agregamos propiedad para el historial
-        /*public List<Operacion> Historial
+        //creamos constructor para inicializar la lista
+        public Calculadora()
         {
-            get
-        }*/
+            dato = 0;
+            historial = new List<Operacion>(); //asi, al hacer new Calculadora() se crea la lista vacia
+        }
+
+        //propiedad para visualizar el historial
+        public List<Operacion> Historial
+        {
+            get => historial;
+        }
 
         //propiedad GET
         public double Resultado
         {
-            get => dato; //cuando se pida "Resultado" devuelve el valor de "dato"
+            get => dato; //cuando se pida "Resultado" devuelve el valor de "dato" 
 
         }   
 
-        //metodos:
+
+        //metodos que agregan al historial:
         public void Sumar(double termino) 
         {
+            //Crear objeto y aregar al historial
+            Operacion op = new Operacion(dato, termino, TipoOperacion.Suma);
+            historial.Add(op);
+
+            //actualizamos dato
             dato += termino;
             
         }
 
         public void Restar(double termino)
         {
+            Operacion op = new Operacion(dato, termino, TipoOperacion.Resta);
+            historial.Add(op);
+
             dato -= termino;
         }
 
         public void Multiplicar(double termino)
         {
+            Operacion op = new Operacion(dato, termino, TipoOperacion.Multiplicacion);
+            historial.Add(op);
+
             dato *= termino;
+
         }
 
-        public void Dividir(double termino)
+        public void Dividir(double termino) //termino == nuevoDato
         {
             if (termino != 0)
             {
+                //si no es 0, lo agregamos al historial
+                Operacion op = new Operacion(dato, termino, TipoOperacion.Division);
+                historial.Add(op);
+
                 dato /= termino;
             }
             else
             {
+                //no agregamos al historial
                 Console.WriteLine("No se puede dividir por cero.");
             }
             
         }
 
-                    /*
-            Ej uso:
-            Calculadora MiInstancia = new Calculadora();
-            MiInstancia.Dividir(10); //divide el dato por 10
-            */
-
         public void Limpiar()
         {
+            Operacion op = new Operacion(dato, 0, TipoOperacion.Limpiar); 
+            historial.Add(op);
+
             dato = 0;
         }
 
